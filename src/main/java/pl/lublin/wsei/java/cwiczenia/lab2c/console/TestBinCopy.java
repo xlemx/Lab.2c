4a69c7ac59ab368d58417ce9bf54ea50949e8881
xlemx <magdalena.lem011@o2.pl>
2022-04-25 18:15:38 +0100

    Ad.5.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2c/console/TestBinCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2c/console/TestBinCopy.java
@@ -1,4 +1,32 @@
 package pl.lublin.wsei.java.cwiczenia.lab2c.console;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 public class TestBinCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream("sample 1920-1280. tiff");
+            out = new FileOutputStream ("img_copy.tiff");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
 }
