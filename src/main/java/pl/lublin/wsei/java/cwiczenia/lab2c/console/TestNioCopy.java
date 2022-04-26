555c8eb0c3c879efac67a916a729cc9e6dfa8a92
xlemx <magdalena.lem011@o2.pl>
2022-04-26 19:48:52 +0100

    Ad.9.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2c/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2c/console/TestNioCopy.java
@@ -1,4 +1,5 @@
 package pl.lublin.wsei.java.cwiczenia.lab2c.console;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,9 +10,9 @@ public class TestNioCopy {
     public static void main(String[] args) throws IOException {
         //Long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime(); //dokladniejszy pomiar
-        Path oryginal= Paths.get( "sample_1920×1280.tiff");
-        Path kopia = Paths.get(  "img_copy.tiff");
-        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+        File oryginal= new File( "sample_1920×1280.tiff");
+        File kopia = new File(  "img_copy.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath(), StandardCopyOption.REPLACE_EXISTING);
         //System.out.println("Czas kopiowania: + (System.currentTimeMillis() - startTime)+" ms");
                 System.out.println("Czas kopiowania:" + (float) (System.nanoTime() - startTime)/1000000+ "ms");
     }
